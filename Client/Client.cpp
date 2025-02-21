cacefab992b45ed50968d4dfd9546f2ebe83c217
Jeeberrr <155396376+jeeberrr@users.noreply.github.com>
2025-02-20 19:41:26 -0500

    Create Client.cpp

---
+++ Client/Client.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include "ClientUtils.h"
+
+int count(std::string string) {
+
+	int count = 1;
+	for (char c : string) {
+		if (c == '\n') {
+			count++;
+		}
+	}
+
+	return count;
+
+}
+
+int main() {
+
+	std::string url{};
+	Socket sock{ Socket() };
+
+	start:
+
+	std::cout << "Welcome, enter your IP web api link: ";
+	std::cin >> url;
+
+	std::string ips{ ApiConnect(url) };
+	int ipcount{ count(ips) };
+	std::cerr << ips << '\n' << "You have " << ipcount << " ips available." << '\n';
+
+	std::cerr << "Enter your command (type help for command list and args)\n(FORMAT: Command argname arg argname arg argname arg): ";
+	std::string command{};
+	std::cin >> command;
+
+	sock.Start();
+	Command::Run(command, ips, sock);
+
+
+}
